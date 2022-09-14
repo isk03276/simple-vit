f4e4b2ab8b63579ff4f00603b729f718743b54ec
isk03276 <isk03276@naver.com>
2022-09-14 11:13:30 +0900

    Fix slice image to patches method

--- utils/image.py
+++ utils/image.py
@@ -18,7 +18,7 @@ def slice_image_to_patches(images: torch.Tensor, patch_size: int, flatten: bool 
     
     images_shape = images.shape
     n_batch, n_channel = images_shape[:2]
-    patches = images.unfold(1, n_channel, n_channel).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size).squeeze()
+    patches = images.unfold(1, n_channel, n_channel).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size).squeeze(dim=1)
     if flatten:
         patches = patches.flatten(start_dim=1, end_dim=2)
         patches = patches.flatten(start_dim=2)
