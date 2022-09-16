4834a6ff14b32e3de6699f0f46ba76e5c0282633
isk03276 <isk03276@naver.com>
2022-09-16 11:04:07 +0900

    Add model save/load methods

---
+++ tests/utils/test_torch.py
@@ -0,0 +1,40 @@
+import shutil
+import sys
+import unittest
+
+import torch
+import torch.nn as nn
+
+try:
+    from vision_transformer.layers import MLPBlock
+    from utils.torch import save_model, load_model
+except ModuleNotFoundError:
+    sys.path.append(".")
+    from vision_transformer.layers import MLPBlock
+    from utils.torch import save_model, load_model
+
+
+class TestTorch(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_model1 = MLPBlock(1)
+        self.test_model2 = MLPBlock(1)
+        self.test_input = torch.ones(1)
+        self.dir_name = "test_dir"
+        self.file_name = "test"
+        
+    def setUp(self):
+        super().setUp()
+        save_model(self.test_model1, self.dir_name, self.file_name)
+        
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(self.dir_name)
+        
+    def test_save_and_load_model(self):
+        load_model(self.test_model2, "test_dir/test")
+        assert self.test_model1(self.test_input) == self.test_model2(self.test_input)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
--- utils/torch.py
+++ utils/torch.py
@@ -1,5 +1,8 @@
+import os
+
 import numpy as np
 import torch
+import torch.nn as nn
 
 
 def tensor_to_array(tensor: torch.Tensor) -> np.ndarray:
@@ -13,3 +16,13 @@ def get_device(device_name: str) -> torch.device:
         print("[Device name error] Use cpu device!")
         device = torch.device("cpu")
     return device
+
+def save_model(model: nn.Module , dir_name: str, file_name: str):
+    os.makedirs(dir_name, exist_ok=True)
+    dir_name = dir_name.strip()
+    if dir_name[-1] != "/":
+        dir_name += "/"
+    torch.save(model.state_dict(), dir_name + file_name)
+
+def load_model(model: nn.Module, path: str):
+    model.load_state_dict(torch.load(path))
