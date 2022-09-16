3a384fedc6fcfb76dc1677ad9d213abfdd819eaf
isk03276 <isk03276@naver.com>
2022-09-16 10:40:48 +0900

    Add config utils

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+import yaml
+
+
+def load_from_yaml(yaml_file_path: str):
+    with open(yaml_file_path, "r") as f:
+        config = yaml.safe_load(f)
+    return config
+
+def save_yaml(dict_data: dict, yaml_file_path: str):
+    with open(yaml_file_path, "w") as f:
+        yaml.safe_dump(dict_data, f)
